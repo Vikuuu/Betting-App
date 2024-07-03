8454919eb6c385e4b6ac25eb3929f17bbf3b9969
Aditya Vikram Singh <adivik672@gmail.com>
2024-07-03 18:41:51 +0530

    Swagger added

--- core/serializers.py
+++ core/serializers.py
@@ -5,3 +5,4 @@ from django.contrib.auth import get_user_model
 class UserAccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
+        fields = "__all__"
