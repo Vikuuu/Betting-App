f1046718e87cba4e1ed39620fd733ce7c1b7f808
Aditya Vikram Singh <adivik672@gmail.com>
2024-07-14 13:42:28 +0530

    unrequired code commented

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UserAccountView
 
 urlpatterns = [
-    path("account/", UserAccountView.as_view(), name="account"),
+    # path("account/", UserAccountView.as_view(), name="account"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,8 @@
-from rest_framework import generics
-from .models import UserAccount
-from .serializers import UserAccountSerializer
+# from rest_framework import generics
+# from .models import UserAccount
+# from .serializers import UserAccountSerializer
 
 
-class UserAccountView(generics.ListCreateAPIView):
-    queryset = UserAccount.objects.all()
-    serializer_class = UserAccountSerializer
+# class UserAccountView(generics.ListCreateAPIView):
+#     queryset = UserAccount.objects.all()
+#     serializer_class = UserAccountSerializer
