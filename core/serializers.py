0b5c1ad1700aee731a0cb4207aaee19295400983
Aditya Vikram Singh <adivik672@gmail.com>
2024-07-14 13:42:28 +0530

    unrequired code commented

--- core/serializers.py
+++ core/serializers.py
@@ -1,8 +1,8 @@
-from rest_framework import serializers
-from django.contrib.auth import get_user_model
+# from rest_framework import serializers
+# from django.contrib.auth import get_user_model
 
 
-class UserAccountSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = get_user_model()
-        fields = "__all__"
+# class UserAccountSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = get_user_model()
+#         fields = "__all__"
